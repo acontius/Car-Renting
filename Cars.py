61a02c59ee85ca1c35db76de25a6a50c24e79fef
acontius <152691439+acontius@users.noreply.github.com>
2024-04-10 12:28:58 +0330

    added the car module And handeled some errors

---
+++ Cars.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self, name, price, seats, quantity):
+        self.name = name
+        self.price = price
+        self.seats = seats
+        self.quantity = quantity
+
+
+car_list = [
+    Car("Ford Fiesta", 120, 4, 8),
+    Car("Mazda2", 100, 4, 10),
+    Car("Audi A4", 200, 4, 3),
+    Car("Lexus Ls", 210, 5, 4),
+    Car("Cadillac CTS", 260, 4, 3),
+    Car("Porsche 911", 400, 2, 2),
+    Car("BMW Z4", 300, 2, 5),
+    Car("Mazda CX-3", 120, 5, 12),
+    Car("Hyundai SANTA FE", 120, 5, 10),
+    Car("Toyota Camary", 100, 4, 10),
+    Car("Amir abas almasi",0,5,1)
+]
--- Menu.py
+++ Menu.py
@@ -1,29 +1,10 @@
+import Cars
+
 #Mohammad Amin Shahabi
 #Computer Scince 1401
 #40111262114
 
 print("Hello Babe")
-
-class Car:
-    def __init__(self, name, price, seats, quantity):
-        self.name = name
-        self.price = price
-        self.seats = seats
-        self.quantity = quantity
-
-car_list = [
-    Car("Ford Fiesta", 120, 4, 8),
-    Car("Mazda2", 100, 4, 10),
-    Car("Audi A4", 200, 4, 3),
-    Car("Lexus Ls", 210, 5, 4),
-    Car("Cadillac CTS", 260, 4, 3),
-    Car("Porsche 911", 400, 2, 2),
-    Car("BMW Z4", 300, 2, 5),
-    Car("Mazda CX-3", 120, 5, 12),
-    Car("Hyundai SANTA FE", 120, 5, 10),
-    Car("Toyota Camary", 100, 4, 10),
-    Car("Amir abas almasi",0,5,1)
-]
 
 invoice_list = []
 
@@ -57,26 +38,30 @@ class Menu:
         print("Item Name          | Price   | Seat   | Quantity")
         print("------------------------------------------------------------------")
 
-        for i, car in enumerate(car_list):
+        for i, car in enumerate(Cars.car_list):
             print(f"{i+1} | {car.name:<17} | {car.price:<7} | {car.seats:<6} | {car.quantity}")
 
-        car_choice = int(input("Select a car: ")) - 1
+        try : 
+            car_choice = int(input("Select a car: ")) - 1
+            if 0 <= car_choice < len(Cars.car_list):
+                self.show_car_details(car_choice)
+                cont = input("Do you want to order another car? (1-Yes, 2-No): ")
 
-        if 0 <= car_choice < len(car_list):
-            self.show_car_details(car_choice)
-            cont = input("Do you want to order another car? (1-Yes, 2-No): ")
-
-            if cont == "1":
-                self.add_car_to_invoice(car_choice)
-            elif cont == "2":
-                self.run_menu()
+                if cont == "1":
+                    self.add_car_to_invoice(car_choice)
+                elif cont == "2":
+                    self.run_menu()
+                else:
+                    print("Invalid choice. Returning to main menu.")
             else:
-                print("Invalid choice. Returning to main menu.")
-        else:
-            print("Invalid car choice. Returning to main menu.")
+                print("Invalid car choice. Returning to main menu.")
+        except(ValueError):
+            print("Invalid input")
+            self.run_menu()
 
+            
     def show_car_details(self, car_index):
-        car = car_list[car_index]
+        car = Cars.car_list[car_index]
         print(f"\nCar Details:")
         print(f"Name: {car.name}")
         print(f"Price: {car.price}$")
@@ -84,7 +69,7 @@ class Menu:
         print(f"Quantity: {car.quantity}")
 
     def add_car_to_invoice(self, car_index):
-        car = car_list[car_index]
+        car = Cars.car_list[car_index]
         invoice_list.append(car)
         print("Car added to invoice.")
 
@@ -134,13 +119,13 @@ class Menu:
             new_car_seats = int(input("Enter the number of seats: "))
             new_car_quantity = int(input("Enter the quantity available: "))
 
-            car_list.append(Car(new_car_name, new_car_price, new_car_seats, new_car_quantity))
+            Cars.car_list.append(Cars.Car(new_car_name, new_car_price, new_car_seats, new_car_quantity))
             print("Car added successfully.")
         else:
             car_to_edit = int(input("Enter the car number to edit: ")) - 1
 
-            if 0 <= car_to_edit < len(car_list):
-                car = car_list[car_to_edit]
+            if 0 <= car_to_edit < len(Cars.car_list):
+                car = Cars.car_list[car_to_edit]
                 print(f"Editing car: {car.name}")
                 car.name = input("Enter the updated car name: ")
                 car.price = int(input("Enter the updated car price: "))
@@ -155,9 +140,9 @@ class Menu:
     def delete_car(self):
         car_to_remove = int(input("Enter the car number to delete: ")) - 1
 
-        if 0 <= car_to_remove < len(car_list):
-            car = car_list[car_to_remove]
-            car_list.remove(car)
+        if 0 <= car_to_remove < len(Cars.car_list):
+            car = Cars.car_list[car_to_remove]
+            Cars.car_list.remove(car)
             print("Car deleted successfully.")
         else:
             print("Invalid car number.")
